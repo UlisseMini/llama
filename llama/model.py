716a1d4c039580c83f7ec6d8e723da81ebbe916c
Ulisse Mini <ulisse.mini@gmail.com>
2023-08-24 13:12:16 +0900

    Fix whitespace removal

--- llama/model.py
+++ llama/model.py
@@ -1,13 +1,17 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+
 import math
 from dataclasses import dataclass
 from typing import Any, Optional, Tuple, List
+
 import torch
 import torch.nn.functional as F
 import torch.nn.init as init
 from torch.nn import Linear, Embedding # fairscale replacements
 from torch import nn
+
+
 @dataclass
 class ModelArgs:
     dim: int = 4096
@@ -18,30 +22,42 @@ class ModelArgs:
     multiple_of: int = 256  # make SwiGLU hidden layer size multiple of large power of 2
     ffn_dim_multiplier: Optional[float] = None
     norm_eps: float = 1e-5
+
     max_batch_size: int = 32
     max_seq_len: int = 2048
+
+
+
 class RMSNorm(torch.nn.Module):
     def __init__(self, dim: int, eps: float = 1e-6):
         super().__init__()
         self.eps = eps
         self.weight = nn.Parameter(torch.ones(dim))
+
     def _norm(self, x):
         return x * torch.rsqrt(x.pow(2).mean(-1, keepdim=True) + self.eps)
+
     def forward(self, x):
         output = self._norm(x.float()).type_as(x)
         return output * self.weight
+
+
 def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
     t = torch.arange(end, device=freqs.device)  # type: ignore
     freqs = torch.outer(t, freqs).float()  # type: ignore
     freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
     return freqs_cis
+
+
 def reshape_for_broadcast(freqs_cis: torch.Tensor, x: torch.Tensor):
     ndim = x.ndim
     assert 0 <= 1 < ndim
     assert freqs_cis.shape == (x.shape[1], x.shape[-1])
     shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
     return freqs_cis.view(*shape)
+
+
 def apply_rotary_emb(
     xq: torch.Tensor,
     xk: torch.Tensor,
@@ -53,6 +69,8 @@ def apply_rotary_emb(
     xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
     xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
     return xq_out.type_as(xq), xk_out.type_as(xk)
+
+
 def apply_rotary_emb_cross(
     xq: torch.Tensor,
     xk: torch.Tensor,
@@ -61,11 +79,17 @@ def apply_rotary_emb_cross(
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
     xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+
     freqs_cis_q = reshape_for_broadcast(freqs_cis_q, xq_)
     freqs_cis_k = reshape_for_broadcast(freqs_cis_k, xk_)
+
     xq_out = torch.view_as_real(xq_ * freqs_cis_q).flatten(3)
     xk_out = torch.view_as_real(xk_ * freqs_cis_k).flatten(3)
+
     return xq_out.type_as(xq), xk_out.type_as(xk)
+
+
+
 def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
     """torch.repeat_interleave(x, dim=2, repeats=n_rep)"""
     bs, slen, n_kv_heads, head_dim = x.shape
@@ -76,6 +100,8 @@ def repeat_kv(x: torch.Tensor, n_rep: int) -> torch.Tensor:
         .expand(bs, slen, n_kv_heads, n_rep, head_dim)
         .reshape(bs, slen, n_kv_heads * n_rep, head_dim)
     )
+
+
 class Attention(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
@@ -85,6 +111,7 @@ class Attention(nn.Module):
         self.n_local_kv_heads = self.n_kv_heads
         self.n_rep = self.n_local_heads // self.n_local_kv_heads
         self.head_dim = args.dim // args.n_heads
+
         self.wq = Linear(
             args.dim,
             args.n_heads * self.head_dim,
@@ -105,6 +132,7 @@ class Attention(nn.Module):
             args.dim,
             bias=False,
         )
+
         self.cache_k = torch.zeros(
             (
                 args.max_batch_size,
@@ -121,6 +149,7 @@ class Attention(nn.Module):
                 self.head_dim,
             )
         ).cuda()
+
     def forward(
         self,
         x: torch.Tensor,
@@ -130,19 +159,26 @@ class Attention(nn.Module):
     ):
         bsz, seqlen, _ = x.shape
         xq, xk, xv = self.wq(x), self.wk(x), self.wv(x)
+
         xq = xq.view(bsz, seqlen, self.n_local_heads, self.head_dim)
         xk = xk.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
         xv = xv.view(bsz, seqlen, self.n_local_kv_heads, self.head_dim)
+
         xq, xk = apply_rotary_emb(xq, xk, freqs_cis=freqs_cis)
+
         self.cache_k = self.cache_k.to(xq)
         self.cache_v = self.cache_v.to(xq)
+
         self.cache_k[:bsz, start_pos : start_pos + seqlen] = xk
         self.cache_v[:bsz, start_pos : start_pos + seqlen] = xv
+
         keys = self.cache_k[:bsz, : start_pos + seqlen]
         values = self.cache_v[:bsz, : start_pos + seqlen]
+
         # repeat k/v heads if n_kv_heads < n_heads
         keys = repeat_kv(keys, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
         values = repeat_kv(values, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+
         xq = xq.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
         keys = keys.transpose(1, 2)
         values = values.transpose(1, 2)
@@ -153,6 +189,7 @@ class Attention(nn.Module):
         output = torch.matmul(scores, values)  # (bs, n_local_heads, seqlen, head_dim)
         output = output.transpose(1, 2).contiguous().view(bsz, seqlen, -1)
         return self.wo(output)
+
 class CrossAttention(nn.Module):
     def __init__(self, args: ModelArgs):
         super().__init__()
@@ -161,6 +198,8 @@ class CrossAttention(nn.Module):
         self.n_local_kv_heads = self.n_kv_heads
         self.n_rep = self.n_local_heads // self.n_local_kv_heads
         self.head_dim = args.dim // args.n_heads
+
+
         self.wq = Linear(
             args.dim,
             args.n_heads * self.head_dim,
@@ -181,6 +220,7 @@ class CrossAttention(nn.Module):
             args.dim,
             bias=False,
         )
+
     def forward(
         self,
         que: torch.Tensor,
@@ -191,16 +231,22 @@ class CrossAttention(nn.Module):
     ):
         que_bsz, que_seqlen, _ = que.shape
         kiv_bsz, kiv_seqlen, _ = kiv.shape
+
         xq, xk, xv = self.wq(que), self.wk(kiv), self.wv(kiv)
+
         xq = xq.view(que_bsz, que_seqlen, self.n_local_heads, self.head_dim)
         xk = xk.view(kiv_bsz, kiv_seqlen, self.n_local_kv_heads, self.head_dim)
         xv = xv.view(kiv_bsz, kiv_seqlen, self.n_local_kv_heads, self.head_dim)
+
         xq, keys = apply_rotary_emb_cross(xq, xk, freqs_cis_q=freqs_cis, freqs_cis_k=ctx_freqs_cis)
+
         keys = xk.view(que_bsz, -1, self.n_local_kv_heads, self.head_dim)
         values = xv.view(que_bsz, -1, self.n_local_kv_heads, self.head_dim)
+
         # repeat k/v heads if n_kv_heads < n_heads
         keys = repeat_kv(keys, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
         values = repeat_kv(values, self.n_rep)  # (bs, seqlen, n_local_heads, head_dim)
+
         xq = xq.transpose(1, 2)  # (bs, n_local_heads, seqlen, head_dim)
         keys = keys.transpose(1, 2)
         values = values.transpose(1, 2)
@@ -211,6 +257,9 @@ class CrossAttention(nn.Module):
         output = torch.matmul(scores, values)  # (bs, n_local_heads, seqlen, head_dim)
         output = output.transpose(1, 2).contiguous().view(que_bsz, que_seqlen, -1)
         return self.wo(output)
+
+
+
 class FeedForward(nn.Module):
     def __init__(
         self,
@@ -225,17 +274,21 @@ class FeedForward(nn.Module):
         if ffn_dim_multiplier is not None:
             hidden_dim = int(ffn_dim_multiplier * hidden_dim)
         hidden_dim = multiple_of * ((hidden_dim + multiple_of - 1) // multiple_of)
+
         self.w1 = Linear(
-            dim, hidden_dim, bias=False,
+            dim, hidden_dim, bias=False, 
         )
         self.w2 = Linear(
-            hidden_dim, dim, bias=False,
+            hidden_dim, dim, bias=False, 
         )
         self.w3 = Linear(
-            dim, hidden_dim, bias=False,
+            dim, hidden_dim, bias=False, 
         )
+
     def forward(self, x):
         return self.w2(F.silu(self.w1(x)) * self.w3(x))
+
+
 class TransformerBlock(nn.Module):
     def __init__(self, layer_id: int, args: ModelArgs):
         super().__init__()
@@ -252,6 +305,7 @@ class TransformerBlock(nn.Module):
         self.layer_id = layer_id
         self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
         self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
     def forward(
         self,
         x: torch.Tensor,
@@ -264,59 +318,76 @@ class TransformerBlock(nn.Module):
         )
         out = h + self.feed_forward.forward(self.ffn_norm(h))
         return out
+
+
 class Transformer(nn.Module):
     def __init__(self, params: ModelArgs):
         super().__init__()
         self.params = params
         self.vocab_size = params.vocab_size
         self.n_layers = params.n_layers
+
         self.tok_embeddings = Embedding(
-            params.vocab_size, params.dim,
+            params.vocab_size, params.dim, 
         )
+
         self.layers = torch.nn.ModuleList()
         for layer_id in range(params.n_layers):
             self.layers.append(TransformerBlock(layer_id, params))
+
         self.encoder_layers = torch.nn.ModuleList()
         for layer_id in range(3):
             self.encoder_layers.append(TransformerBlock(layer_id, params))
+
         self.cross_attn_layers = torch.nn.ModuleList()
         for layer_id in range(9, 30, 3):
             self.cross_attn_layers.append(CrossAttention(params))
+
         self.norm = RMSNorm(params.dim, eps=params.norm_eps)
         self.output = Linear(
-            params.dim, params.vocab_size, bias=False,
+            params.dim, params.vocab_size, bias=False, 
         )
+
         self.freqs_cis = precompute_freqs_cis(
             self.params.dim // self.params.n_heads, self.params.max_seq_len * 2
         )
+
     def forward(self, tokens: torch.Tensor, context_tokens: torch.Tensor, start_pos: int):
-        # TODO: Clean up no context case or remove. way too many if statements...
+        # TODO: Clean up no context handling to be cleaner
         ctx = tuple(context_tokens.shape) != (0,)
+
         bsz, seqlen = tokens.shape
         if ctx:
             _bsz, context_seqlen = context_tokens.shape
+
         h = self.tok_embeddings(tokens)
         if ctx:
             ch = self.tok_embeddings(context_tokens)
+
         self.freqs_cis = self.freqs_cis.to(h.device)
         freqs_cis = self.freqs_cis[start_pos : start_pos + seqlen]
+
         if ctx:
             context_freqs_cis = self.freqs_cis[:context_seqlen]
+
         mask = None
         if seqlen > 1:
             mask = torch.full(
                 (1, 1, seqlen, seqlen), float("-inf"), device=tokens.device
             )
             mask = torch.triu(mask, diagonal=start_pos + 1).type_as(h)
+
         if ctx:
             for encoder_layer in self.encoder_layers:
                 ch = encoder_layer(ch, 0, context_freqs_cis, None)
+        
         for layer in self.layers:
             h = layer(h, start_pos, freqs_cis, mask)
             # if layer 9, 12, ..., 30 do cross attention with encoded context
             if ctx and layer.layer_id in range(9, 30, 3):
                 h = h + self.cross_attn_layers[layer.layer_id//3 - 3](h, ch, freqs_cis, context_freqs_cis, None)
                 # TODO: Norm after cross attn?
+
         h = self.norm(h)
         output = self.output(h).float()
         return output
